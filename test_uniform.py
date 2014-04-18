b933e51d35e2796dd7d8ffc0c07f114fd8e82c2c
Li Ge <lge@ucsd.edu>
2014-04-18 01:22:20 -0700

    use generator

--- test_uniform.py
+++ test_uniform.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python
 import sys
 from cross_review import *
 
-def generate_hist(num_of_ids = 40, N = 2, indx = 0, num_of_tests = 1000):
+def generate_hist(num_of_ids = 20, N = 2, indx = 0, num_of_tests = 1000):
     ids = [str(i) for i in range(num_of_ids)]
     bins = [0 for i in range(num_of_ids)]
     ten_percent = int(num_of_tests/10)
 
+    yield (0.0, bins)
     for i in range(num_of_tests):
         g = Graph(ids, N)
         g.ford_fulkerson_max_flow()
@@ -16,11 +18,10 @@ def generate_hist(num_of_ids = 40, N = 2, indx = 0, num_of_tests = 1000):
             bins[int(reviewee)] += 1
 
         if (i + 1) % ten_percent == 0:
-            print '# %2.1f%%' % (100.0 * (i+1) / num_of_tests)
-            sys.stdout.flush()
-
-    return bins
+            yield (100.0 * (i+1) / num_of_tests, bins)
 
 if __name__ == '__main__':
-    bins = generate_hist()
+    for percent, bins in generate_hist():
+        print '# %2.1f%%' % percent
+
     print '\n'.join(map(str, bins))
